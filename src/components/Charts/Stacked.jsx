32210ec613ecd7759c02db601530bc0a5e00d3f2
Zahidul Haque <jahidulhaque767@gmail.com>
2023-02-18 03:35:57 +0600

    stacked chart complete

--- src/components/Charts/Stacked.jsx
+++ src/components/Charts/Stacked.jsx
@@ -21,7 +21,7 @@ const Stacked = ({ width, height }) => {
     <ChartComponent
       width={width}
       height={height}
-      id="stack chart"
+      id="charts"
       primaryXAxis={stackedPrimaryXAxis}
       primaryYAxis={stackedPrimaryYAxis}
       chartArea={{ border: { width: 0 } }}
