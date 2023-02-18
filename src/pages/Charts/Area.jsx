43cfbf52197a6b99ce3a7605369a57adf489e578
Zahidul Haque <jahidulhaque767@gmail.com>
2023-02-19 04:02:27 +0600

    complete area chart page

--- src/pages/Charts/Area.jsx
+++ src/pages/Charts/Area.jsx
@@ -1,7 +1,41 @@
 import React from "react";
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  DateTime,
+  Legend,
+  SplineAreaSeries,
+} from "@syncfusion/ej2-react-charts";
+
+import {
+  areaCustomSeries,
+  areaPrimaryYAxis,
+  areaPrimaryXAxis,
+} from "../../data/dummy";
+
+import { Header } from "../../components";
 
 const Area = () => {
-  return <div>Area</div>;
+  return (
+    <div className="m-4 md:m-10 mt-24 p-10 bg-white rounded-3xl">
+      <Header category="Chart" title="Area Chart" />
+      <ChartComponent
+        id="line-chart"
+        primaryXAxis={areaPrimaryXAxis}
+        primaryYAxis={areaPrimaryYAxis}
+        tooltip={{ enable: true }}
+      >
+        <Inject services={[SplineAreaSeries, DateTime, Legend]} />
+        <SeriesCollectionDirective>
+          {areaCustomSeries.map((item, ind) => (
+            <SeriesDirective key={ind} {...item} />
+          ))}
+        </SeriesCollectionDirective>
+      </ChartComponent>
+    </div>
+  );
 };
 
 export default Area;
